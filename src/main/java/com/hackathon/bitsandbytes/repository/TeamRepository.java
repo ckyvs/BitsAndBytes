564240db8af0398634cfd7530cc784254e33fc38
aditya <adityagautamm@rediffmail.com>
2020-10-07 17:38:19 +0530

    annotation added

--- src/main/java/com/hackathon/bitsandbytes/repository/TeamRepository.java
+++ src/main/java/com/hackathon/bitsandbytes/repository/TeamRepository.java
@@ -1,7 +1,13 @@
 package com.hackathon.bitsandbytes.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Transactional
 public interface TeamRepository extends JpaRepository<TeamRepository,Long> {
 
 }
