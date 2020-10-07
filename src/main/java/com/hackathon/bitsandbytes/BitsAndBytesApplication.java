949c4123206822a3c0570260a430a4aaca4373ef
YVS Chakri <chakriyvs@gmail.com>
2020-10-07 14:58:45 +0530

    first commit

---
+++ src/main/java/com/hackathon/bitsandbytes/BitsAndBytesApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bitsandbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitsAndBytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitsAndBytesApplication.class, args);
+	}
+
+}
