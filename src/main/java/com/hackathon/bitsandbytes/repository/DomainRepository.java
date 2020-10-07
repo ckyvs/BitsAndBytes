7bffa2063ca82e50223a9aedabbab198b9328163
aditya <adityagautamm@rediffmail.com>
2020-10-07 17:37:56 +0530

    annotations added

--- src/main/java/com/hackathon/bitsandbytes/repository/DomainRepository.java
+++ src/main/java/com/hackathon/bitsandbytes/repository/DomainRepository.java
@@ -1,7 +1,12 @@
 package com.hackathon.bitsandbytes.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
 public interface DomainRepository extends JpaRepository<DomainRepository,Long>{
 
 }
--- src/main/java/com/hackathon/bitsandbytes/repository/VacancyRepository.java
+++ src/main/java/com/hackathon/bitsandbytes/repository/VacancyRepository.java
@@ -1,7 +1,14 @@
 package com.hackathon.bitsandbytes.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@Transactional
 public interface VacancyRepository extends JpaRepository<VacancyRepository,Long> {
 
 }
