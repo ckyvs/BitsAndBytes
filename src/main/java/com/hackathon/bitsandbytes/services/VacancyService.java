60b92306b79aa541202257c0e96cbbcbefade4ad
Yashwant <yashwanthdesu@gmail.com>
2020-10-07 19:26:26 +0530

    Added methods to the VacancyService

--- src/main/java/com/hackathon/bitsandbytes/services/VacancyService.java
+++ src/main/java/com/hackathon/bitsandbytes/services/VacancyService.java
@@ -1,8 +1,12 @@
 package com.hackathon.bitsandbytes.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hackathon.bitsandbytes.entity.Vacancy;
 import com.hackathon.bitsandbytes.repository.VacancyRepository;
 
 @Service
@@ -12,6 +16,31 @@ public class VacancyService {
 	VacancyRepository vacancyRepository;
 	
 	
+	 List<Vacancy> listOfVacancies = new ArrayList<Vacancy>();
 	
+	 
+	public List<Vacancy> getAllVacancies(){
+		List<Vacancy> receivedAllVacancies = vacancyRepository.findAll();
+		return receivedAllVacancies;
+	}
+	 
+	 
+	//Adding a single vacancy.
+	public Vacancy addVacancy(Vacancy vacancy) {
+		Vacancy savedvacancy = vacancyRepository.save(vacancy);
+		return savedvacancy;
+	}
+	
+	//Adding multiple vacancies at a time;
+	public List<Vacancy> addMultipleVacancies(List<Vacancy> vacancies) {
+		List<Vacancy> savedVacancies = (List<Vacancy>) vacancyRepository.saveAll(vacancies);
+		return savedVacancies;
+	}
+	
+	//Update the vacancy in the repository
+	public Vacancy updateVacancyDetails(Vacancy updateVacancyDetails) {
+		Vacancy updatedVacancy = vacancyRepository.save(updateVacancyDetails);
+		return updatedVacancy;
+	}
 	
 }
