a108cb75df05c2c91a0ec00ea259d4324e2e587a
YVS Chakri <chakriyvs@gmail.com>
2020-10-07 15:31:00 +0530

    fixed security

--- src/main/java/com/hackathon/bitsandbytes/security/JwtInMemoryUserDetailsService.java
+++ src/main/java/com/hackathon/bitsandbytes/security/JwtInMemoryUserDetailsService.java
@@ -2,35 +2,43 @@ package com.hackathon.bitsandbytes.security;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.capstone.soar.domain.Employee;
-import com.capstone.soar.repository.EmployeeRepository;
 
 @Service
 public class JwtInMemoryUserDetailsService implements UserDetailsService {
 	
-	@Autowired
-	EmployeeRepository employeeRepo;
-
   static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
+  static {
+    inMemoryUserList.add(new JwtUserDetails(1L, "admin",
+        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ADMIN"));
+  }
 
   @Override
   public UserDetails loadUserByUsername(String username) {
-    Employee employee = employeeRepo.getEmployeeByEmail(username);
-    if (employee == null) {
-      throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
-    }
+//    Employee employee = employeeRepo.getEmployeeByEmail(username);
+//    if (employee == null) {
+//      throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+//    }
+//
+//    return new JwtUserDetails(employee.getId(), employee.getEmail(), employee.getPassword(), employee.getRole().getRole());
+//  }
 
-    return new JwtUserDetails(employee.getId(), employee.getEmail(), employee.getPassword(), employee.getRole().getRole());
+	  Optional<JwtUserDetails> findFirst = inMemoryUserList.stream()
+				.filter(user -> user.getUsername().equals(username)).findFirst();
+
+		if (!findFirst.isPresent()) {
+			throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+		}
+
+		return findFirst.get();
   }
-
 }
 
 
